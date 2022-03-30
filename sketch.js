801e7aba43e1e34334c53cbbfc1b94b9f2306a24
talyssoncrivelari <96204895+talyssoncrivelari@users.noreply.github.com>
2022-03-29 22:05:52 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,17 +1,22 @@
 let rect1, rect1IMG;
+let play, playIMG;
 
 function preload() {
   rect1IMG = loadImage("RBA0.png");
+  playIMG = loadImage("PlayBotton0.png");
 }
 
 function setup() {
 
 createCanvas(windowWidth, windowHeight);
 rect1 = createSprite(windowWidth - 100, windowHeight/2);
-rect1.shapeColor = "white";
 rect1.addImage(rect1IMG);
 rect1.scale = windowHeight - 1024.5;
 rect1.rotation = 270;
+play = createSprite(windowWidth - 625, windowHeight/2);
+play.addImage(playIMG);
+play.scale = windowHeight - 1024.12;
+play.rotation = 270;
 }
 
 function draw() {
